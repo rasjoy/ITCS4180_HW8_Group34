09334c0541367eb095f988d3bf5e8e8ef393c287
rasjoy <rasmussenjoy@gmail.com>
2017-04-02 14:53:30 -0400

    Added menu and put api key in MainActivity

---
+++ app/src/main/java/com/example/joyrasmussen/hw8_group34/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.joyrasmussen.hw8_group34;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    static final String API_Key = "zzAnkMX16zZhnHzpY2LvaJFp69R7JDM4";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
