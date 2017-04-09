6ed71e77409982e5a1a79b5ce67fd1c4765dc8fd
rasjoy <rasmussenjoy@gmail.com>
2017-04-08 21:16:30 -0400

    no more conflicts

--- app/src/main/java/com/example/joyrasmussen/hw8_group34/ForcastAdapater.java
+++ app/src/main/java/com/example/joyrasmussen/hw8_group34/ForcastAdapater.java
@@ -1,10 +1,16 @@
 package com.example.joyrasmussen.hw8_group34;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.List;
 
 /**
  * Created by joyrasmussen on 4/8/17.
@@ -12,17 +18,24 @@ import android.widget.TextView;
 
 public class ForcastAdapater extends RecyclerView.Adapter<ForcastAdapater.ForcastHolder> {
     private Context mContext;
-    private List<OneDayForast>;
-    private SharedPreference preference;
+    private List<OneDayForecast> data;
+    private SharedPreferences mPreference;
+
+    public ForcastAdapater(Context mContext, List<OneDayForecast> data) {
+        this.mContext = mContext;
+        this.data = data;
+        this.mPreference = PreferenceManager.getDefaultSharedPreferences(mContext);
+    }
 
     @Override
     public ForcastHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.five_days_layout, parent, false);
+            return new ForcastHolder(view);
     }
 
     @Override
     public void onBindViewHolder(ForcastHolder holder, int position) {
-
+       OneDayForecast forecast = data.get(position);
     }
 
     @Override
