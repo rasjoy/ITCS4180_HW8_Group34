bc8b690f282220f4d9959bb2a8317e9442f82da4
Clay Holt <clayholt@gmx.com>
2017-04-08 21:52:57 -0400

    Merge branch 'master' of /home/clay/Desktop/android/ITCS4180_HW8_Group34 with conflicts.

--- app/src/main/java/com/example/joyrasmussen/hw8_group34/ForcastAdapater.java
+++ app/src/main/java/com/example/joyrasmussen/hw8_group34/ForcastAdapater.java
@@ -3,7 +3,6 @@ package com.example.joyrasmussen.hw8_group34;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
-
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,23 +10,22 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.squareup.picasso.Picasso;
-
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by joyrasmussen on 4/8/17.
  */
 
-public class ForcastAdapater extends RecyclerView.Adapter<ForcastAdapater.ForcastHolder> implements View.OnClickListener{
+public class ForcastAdapater extends RecyclerView.Adapter<ForcastAdapater.ForcastHolder> {
     private Context mContext;
     private List<OneDayForecast> data;
     private SharedPreferences mPreference;
-    private DetailUpdater detail;
 
-    public ForcastAdapater(Context mContext, DetailUpdater detailUpdater,  List<OneDayForecast> data) {
+
+
+    public ForcastAdapater(Context mContext, List<OneDayForecast> data) {
         this.mContext = mContext;
-        this.detail = detailUpdater;
         this.data = data;
         this.mPreference = PreferenceManager.getDefaultSharedPreferences(mContext);
     }
@@ -40,24 +38,12 @@ public class ForcastAdapater extends RecyclerView.Adapter<ForcastAdapater.Forcas
 
     @Override
     public void onBindViewHolder(ForcastHolder holder, int position) {
-        OneDayForecast forecast = data.get(position);
-        String imageID = forecast.getDayPicture();
-        if(!imageID.isEmpty()) {
-            imageID = imageID.length() == 1 ? "0" + imageID : imageID;
-            Picasso.with(mContext).load(MainActivity.ICON.replace("{Image_ID}", imageID)).into(holder.image);
-        }
-        holder.date.setText(forecast.getDate());
-
+       OneDayForecast forecast = data.get(position);
     }
 
     @Override
     public int getItemCount() {
-        return data.size();
-    }
-
-    @Override
-    public void onClick(View v) {
-        detail.detailUpdate(v);
+        return 0;
     }
 
     static class ForcastHolder extends RecycViewHolder{
@@ -71,8 +57,5 @@ public class ForcastAdapater extends RecyclerView.Adapter<ForcastAdapater.Forcas
             image = (ImageView) itemView.findViewById(R.id.imageRecycler);
 
         }
-    }
-    interface DetailUpdater{
-        void detailUpdate(View v);
     }
 }
